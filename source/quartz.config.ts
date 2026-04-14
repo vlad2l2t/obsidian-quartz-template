68cddcdfb19e355f8a3cb60035715a30a6327212
Vlad. <vlad.vallotton@gmail.com>
2026-04-14 16:19:43 +0200

    Updates

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Stage de 1ere année - Vladimir Vallotton",
+    pageTitle: "Stage Ayesa",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
